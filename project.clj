aed9fc084b6e7e09cb080749b764b1505cbde856
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-17 20:22:29 +0200

    project: Add lein-cljfmt as dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject advent-of-code "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main advent-of-code.core
-  :repl-options {:init-ns advent-of-code.core})
+  :repl-options {:init-ns advent-of-code.core}
+  :plugins [[lein-cljfmt "0.8.0"]])
