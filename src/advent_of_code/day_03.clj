02d6dbff2384a105bd7c47da8ace68ae0781486e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-18 23:57:41 +0200

    day_03/part-1: Solve Part One of Day 3

--- src/advent_of_code/day_03.clj
+++ src/advent_of_code/day_03.clj
@@ -1,9 +1,18 @@
-(ns advent-of-code.day-03)
+(ns advent-of-code.day-03
+  (:require [advent-of-code.util :as util]))
 
 (defn part-1
   "Day 03 Part 1"
-  [input]
-  input)
+  [input] (let [loc (util/parse-int input)
+                radius (int (/ (Math/ceil (Math/sqrt loc)) 2))
+                closest (Math/pow (- (* radius 2) 1) 2)]
+            (->>
+             (range 1 11 2)
+             (map #(+ closest (* radius %)))
+             (map #(Math/abs (- loc %)))
+             (apply min)
+             (+ radius)
+             (int))))
 
 (defn part-2
   "Day 03 Part 2"
--- test/advent_of_code/day_03_test.clj
+++ test/advent_of_code/day_03_test.clj
@@ -3,9 +3,26 @@
             [advent-of-code.day-03 :refer [part-1 part-2]]
             [clojure.java.io :refer [resource]]))
 
-(deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-03-example.txt")))))))
+(deftest part-1-example-1
+  (is (= 0 (part-1 "1"))))
+
+(deftest part-1-example-2
+  (is (= 3 (part-1 "12"))))
+
+(deftest part-1-example-3
+  (is (= 2 (part-1 "23"))))
+
+(deftest part-1-example-4
+  (is (= 31 (part-1 "1024"))))
+
+(deftest part-1-example-5
+  (is (= 5 (part-1 "26"))))
+
+(deftest part-1-example-6
+  (is (= 3 (part-1 "28"))))
+
+(deftest part-1-example-7
+  (is (= 6 (part-1 "49"))))
 
 (deftest part-2-example
   (let [expected ""]
