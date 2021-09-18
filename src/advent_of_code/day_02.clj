1f7ffb9435f2df91d92efa3f417926e77c01b810
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-18 14:13:31 +0200

    util: Create and use a new `util` lib

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,14 +1,13 @@
 (ns advent-of-code.day-02
-  (:require [clojure.string :refer [split split-lines]]))
-
-(defn parse-int [x] (Integer/parseInt x))
+  (:require [advent-of-code.util :as util]
+            [clojure.string :refer [split split-lines]]))
 
 (defn part-1
   "Day 02 Part 1"
   [input] (->>
            (split-lines input)
            (map #(split % #"\s+"))
-           (map #(map parse-int %))
+           (map #(map util/parse-int %))
            (map #(- (apply max %) (apply min %)))
            (reduce +)))
 
---
+++ src/advent_of_code/util.clj
@@ -0,0 +1,5 @@
+(ns advent-of-code.util)
+
+(defn inspect [x] (prn x) x)
+
+(defn parse-int [x] (Integer/parseInt x))
