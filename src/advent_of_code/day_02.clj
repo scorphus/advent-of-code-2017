5272cbc98b9d610b5c5786e7027b53b49dbbf890
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-18 13:25:14 +0200

    day_02/part-1: Solve Part One of Day 2

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,9 +1,16 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+  (:require [clojure.string :refer [split split-lines]]))
+
+(defn parse-int [x] (Integer/parseInt x))
 
 (defn part-1
   "Day 02 Part 1"
-  [input]
-  input)
+  [input] (->>
+           (split-lines input)
+           (map #(split % #"\s+"))
+           (map #(map parse-int %))
+           (map #(- (apply max %) (apply min %)))
+           (reduce +)))
 
 (defn part-2
   "Day 02 Part 2"
--- test/advent_of_code/day_02_test.clj
+++ test/advent_of_code/day_02_test.clj
@@ -1,12 +1,12 @@
 (ns advent-of-code.day-02-test
   (:require [clojure.test :refer [deftest testing is]]
             [advent-of-code.day-02 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :refer [trimr]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-02-example.txt")))))))
+  (is (= 18 (part-1 (trimr (slurp (resource "day-02-example.txt")))))))
 
-(deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-02-example.txt")))))))
+;; (deftest part-2-example
+;;   (let [expected ""]
+;;     (is (= expected (part-2 (trimr (slurp (resource "day-02-example.txt"))))))))
