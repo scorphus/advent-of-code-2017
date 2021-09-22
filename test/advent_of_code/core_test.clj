c73a13a91eb5ee19319aa10803868e1d2efebb44
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-23 00:26:27 +0200

    core: Test `-main` when no function is found

--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -6,3 +6,9 @@
   (with-redefs [advent-of-code.day-01/part-1 (fn [_] "this-is-part-1")
                 println identity]
     (is (= "this-is-part-1" (-main "01" "1")))))
+
+(deftest test-main-not-found
+  (try
+    (-main "01" "0")
+    (throw (Exception. "shouldn't reach this line"))
+    (catch AssertionError _ _)))
