aca8824ab3ba4ee25183638263deda66212d405d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-23 13:05:33 +0200

    day_04/part-1: Solve Part One of Day 4

--- src/advent_of_code/day_04.clj
+++ src/advent_of_code/day_04.clj
@@ -1,9 +1,13 @@
-(ns advent-of-code.day-04)
+(ns advent-of-code.day-04
+  (:require [clojure.string :refer [split split-lines]]))
 
 (defn part-1
   "Day 04 Part 1"
-  [input]
-  input)
+  [input] (->>
+           (split-lines input)
+           (map #(split % #"\s+"))
+           (filter #(apply distinct? %))
+           (count)))
 
 (defn part-2
   "Day 04 Part 2"
--- test/advent_of_code/day_04_test.clj
+++ test/advent_of_code/day_04_test.clj
@@ -1,12 +1,12 @@
 (ns advent-of-code.day-04-test
   (:require [clojure.test :refer [deftest is]]
             [advent-of-code.day-04 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :refer [trimr]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-04-example.txt")))))))
+  (is (= 2 (part-1 (trimr (slurp (resource "day-04-example.txt")))))))
 
 (deftest part-2-example
   (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-04-example.txt")))))))
+    (is (not= expected (part-2 (trimr (slurp (resource "day-04-example.txt"))))))))
