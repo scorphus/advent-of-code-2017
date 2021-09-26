4811547db3cf20c958622cfeea2fa94560f0cb59
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-26 21:53:03 +0200

    day_08/part-1: Solve Part One of Day 8

--- src/advent_of_code/day_08.clj
+++ src/advent_of_code/day_08.clj
@@ -1,9 +1,34 @@
-(ns advent-of-code.day-08)
+(ns advent-of-code.day-08
+  (:require [clojure.string :refer [split split-lines]]
+            [advent-of-code.util :as util]))
+
+(defn evaluate
+  [op a b] (case op
+             ">" (> a b)
+             "<" (< a b)
+             "<=" (<= a b)
+             ">=" (>= a b)
+             "==" (= a b)
+             "!=" (not= a b)
+             "inc" (+ a b)
+             "dec" (- a b)))
+
+(defn evaluate-instructions
+  [registers [a op1 b _ c op2 d]]
+  (let [[a-val c-val] (map #(get registers % 0) [a c])
+        [b-val d-val] (map util/parse-int [b d])]
+    (if (evaluate op2 c-val d-val)
+      (assoc registers a (evaluate op1 a-val b-val))
+      registers)))
 
 (defn part-1
   "Day 08 Part 1"
-  [input]
-  input)
+  [input] (->>
+           (split-lines input)
+           (map #(split % #"\s+"))
+           (reduce evaluate-instructions {})
+           (vals)
+           (apply max)))
 
 (defn part-2
   "Day 08 Part 2"
--- test/advent_of_code/day_08_test.clj
+++ test/advent_of_code/day_08_test.clj
@@ -4,9 +4,7 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-08-example.txt")))))))
+  (is (= 1 (part-1 (slurp (resource "day-08-example.txt"))))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-08-example.txt")))))))
+  (is (not= "" (part-2 (slurp (resource "day-08-example.txt"))))))
