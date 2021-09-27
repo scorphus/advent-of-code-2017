b53478b06939a1a2c72c46a1d342754d2f2e1535
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-27 14:38:10 +0200

    day_09/part-1: Solve Part One of Day 9

--- src/advent_of_code/day_09.clj
+++ src/advent_of_code/day_09.clj
@@ -1,9 +1,18 @@
-(ns advent-of-code.day-09)
+(ns advent-of-code.day-09
+  (:require [clojure.string :refer [replace]]))
+
+(def replace-pairs "Pairs of `match` and `replacement`" [[#"!." ""]
+                                                         [#"<.*?>" ""]
+                                                         [#"\{" "["]
+                                                         [#"\}" "]"]])
+
+(defn find-score
+  [score group] (apply + score (map (partial find-score (inc score)) group)))
 
 (defn part-1
   "Day 09 Part 1"
-  [input]
-  input)
+  [input] (let [input (reduce (fn [s [m r]] (replace s m r)) input replace-pairs)]
+            (apply + 1 (map (partial find-score 2) (load-string input)))))
 
 (defn part-2
   "Day 09 Part 2"
--- test/advent_of_code/day_09_test.clj
+++ test/advent_of_code/day_09_test.clj
@@ -1,12 +1,30 @@
 (ns advent-of-code.day-09-test
   (:require [clojure.test :refer [deftest is]]
-            [advent-of-code.day-09 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [advent-of-code.day-09 :refer [part-1 part-2]]))
 
-(deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-09-example.txt")))))))
+(deftest part-1-example-1
+  (is (= 1 (part-1 "{}"))))
+
+(deftest part-1-example-2
+  (is (= 6 (part-1 "{{{}}}"))))
+
+(deftest part-1-example-3
+  (is (= 5 (part-1 "{{},{}}"))))
+
+(deftest part-1-example-4
+  (is (= 16 (part-1 "{{{},{},{{}}}},"))))
+
+(deftest part-1-example-5
+  (is (= 1 (part-1 "{<a>,<a>,<a>,<a>}"))))
+
+(deftest part-1-example-6
+  (is (= 9 (part-1 "{{<ab>},{<ab>},{<ab>},{<ab>}}"))))
+
+(deftest part-1-example-7
+  (is (= 9 (part-1 "{{<!!>},{<!!>},{<!!>},{<!!>}}"))))
+
+(deftest part-1-example-8
+  (is (= 3 (part-1 "{{<a!>},{<a!>},{<a!>},{<ab>}}"))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-09-example.txt")))))))
+  (is (= "" (part-2 ""))))
