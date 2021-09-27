30f0e8812372452ab76869bfb3e94f4063aa8b58
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-27 22:11:25 +0200

    day_10/part-1: Solve Part One of Day 10

--- src/advent_of_code/day_10.clj
+++ src/advent_of_code/day_10.clj
@@ -1,9 +1,26 @@
-(ns advent-of-code.day-10)
+(ns advent-of-code.day-10
+  (:require [clojure.string :refer [split]]
+            [advent-of-code.util :as util]))
+
+(defn rev-skip
+  "Reverses and skips the list"
+  [the-list [skip length]]
+  (let [size (count the-list)
+        the-list (concat
+                  (take (- size length) (drop length (cycle the-list)))
+                  (reverse (take length (cycle the-list))))]
+    (take size (drop skip (cycle the-list)))))
 
 (defn part-1
   "Day 10 Part 1"
-  [input]
-  input)
+  ([input] (part-1 input 256))
+  ([input size]
+   (let [lengths (map util/parse-int (split input #","))
+         the-list (reduce rev-skip (range size) (map-indexed vector lengths))
+         skips (/ (* (count lengths) (- (count lengths) 1)) 2)
+         droppage (- size (mod (apply + skips lengths) size))
+         [a b] (drop droppage the-list)]
+     (* a b))))
 
 (defn part-2
   "Day 10 Part 2"
