4c7d8784c89661aa15e86c4417f891c0ecc32dc7
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-27 19:29:07 +0200

    day_09/part-1: Declare `replace-pairs` locally

--- src/advent_of_code/day_09.clj
+++ src/advent_of_code/day_09.clj
@@ -2,17 +2,16 @@
   (:refer-clojure :exclude [replace])
   (:require [clojure.string :refer [replace]]))
 
-(def replace-pairs "Pairs of `match` and `replacement`" [[#"!." ""]
-                                                         [#"<.*?>" ""]
-                                                         [#"\{" "["]
-                                                         [#"\}" "]"]])
-
 (defn find-score
   [score group] (apply + score (map (partial find-score (inc score)) group)))
 
 (defn part-1
   "Day 09 Part 1"
-  [input] (let [input (reduce (fn [s [m r]] (replace s m r)) input replace-pairs)]
+  [input] (let [replace-pairs [[#"!." ""]
+                               [#"<.*?>" ""]
+                               [#"\{" "["]
+                               [#"\}" "]"]]
+                input (reduce (fn [s [m r]] (replace s m r)) input replace-pairs)]
             (apply + 1 (map (partial find-score 2) (load-string input)))))
 
 (defn part-2
