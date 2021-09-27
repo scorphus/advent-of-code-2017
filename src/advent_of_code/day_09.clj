3373e0173f81ae6fd62c414538c6e969410a3e48
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-27 19:31:08 +0200

    day_09: Require `clojure.string` as `str`

--- src/advent_of_code/day_09.clj
+++ src/advent_of_code/day_09.clj
@@ -1,6 +1,5 @@
 (ns advent-of-code.day-09
-  (:refer-clojure :exclude [replace])
-  (:require [clojure.string :refer [replace]]))
+  (:require [clojure.string :as str]))
 
 (defn find-score
   [score group] (apply + score (map (partial find-score (inc score)) group)))
@@ -11,11 +10,11 @@
                                [#"<.*?>" ""]
                                [#"\{" "["]
                                [#"\}" "]"]]
-                input (reduce (fn [s [m r]] (replace s m r)) input replace-pairs)]
+                input (reduce (fn [s [m r]] (str/replace s m r)) input replace-pairs)]
             (find-score 1 (load-string input))))
 
 (defn part-2
   "Day 09 Part 2"
-  [input] (let [input (replace input #"!." "")
+  [input] (let [input (str/replace input #"!." "")
                 matches (re-seq #"<(.*?)>" input)]
             (reduce #(+ %1 (count (second %2))) 0 matches)))
