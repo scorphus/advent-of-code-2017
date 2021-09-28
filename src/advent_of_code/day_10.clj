9e9586c38174b89ad2deed516086f67795fb209d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-28 13:59:04 +0200

    day_10/part-1: Slightly improve readability

--- src/advent_of_code/day_10.clj
+++ src/advent_of_code/day_10.clj
@@ -17,10 +17,9 @@
   ([input size]
    (let [lengths (map util/parse-int (split input #","))
          the-list (reduce rev-skip (range size) (map-indexed vector lengths))
-         skips (/ (* (count lengths) (- (count lengths) 1)) 2)
-         droppage (- size (mod (apply + skips lengths) size))
-         [a b] (drop droppage the-list)]
-     (* a b))))
+         skips (* (count lengths) (dec (count lengths)) 1/2)
+         droppage (- size (mod (apply + skips lengths) size))]
+     (apply * (take 2 (drop droppage the-list))))))
 
 (defn part-2
   "Day 10 Part 2"
