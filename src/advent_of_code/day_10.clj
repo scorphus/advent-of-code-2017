f0ab314dcf4a4010a60482fbc5a5ca8574c24631
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-28 20:50:53 +0200

    day_10: Extract common code to a function

--- src/advent_of_code/day_10.clj
+++ src/advent_of_code/day_10.clj
@@ -11,33 +11,36 @@
                   (reverse (take length (cycle the-list))))]
     (take size (drop skip (cycle the-list)))))
 
+(defn knot-hash
+  "Runs a given number of rounds of the Knot Hash"
+  [lengths size rounds]
+  (let [acc-len-size (* rounds (count lengths))
+        the-list (reduce rev-skip
+                         (range size)
+                         (map vector (range acc-len-size) (cycle lengths)))
+        forwards (* rounds (apply + lengths))
+        skips (* acc-len-size (dec acc-len-size) 1/2)
+        droppage (- size (mod (+ forwards skips) size))]
+    (->>
+     (cycle the-list)
+     (drop droppage)
+     (take size))))
+
 (defn part-1
   "Day 10 Part 1"
-  ([input] (part-1 input 256))
-  ([input size]
-   (let [lengths (mapv util/parse-int (split input #","))
-         the-list (reduce rev-skip (range size) (map-indexed vector lengths))
-         forwards (apply + lengths)
-         skips (* (count lengths) (dec (count lengths)) 1/2)
-         droppage (- size (mod (+ forwards skips) size))]
-     (apply * (take 2 (drop droppage the-list))))))
+  ([input] (part-1 input 256 1))
+  ([input size] (part-1 input size 1))
+  ([input size rounds]
+   (let [lengths (mapv util/parse-int (split input #","))]
+     (apply * (take 2 (knot-hash lengths size rounds))))))
 
 (defn part-2
   "Day 10 Part 2"
   ([input] (part-2 input 256 64 16))
   ([input size rounds blocks]
-   (let [lengths (into (mapv byte input) [17 31 73 47 23])
-         acc-len-size (* rounds (count lengths))
-         the-list (reduce rev-skip
-                          (range size)
-                          (map vector (range acc-len-size) (cycle lengths)))
-         forwards (* rounds (apply + lengths))
-         skips (* acc-len-size (dec acc-len-size) 1/2)
-         droppage (- size (mod (+ forwards skips) size))]
+   (let [lengths (into (mapv byte input) [17 31 73 47 23])]
      (->>
-      (cycle the-list)
-      (drop droppage)
-      (take size)
+      (knot-hash lengths size rounds)
       (partition blocks)
       (map #(apply bit-xor %))
       (map #(format "%02x" %))
