288d772850ba9274552dc93017297627b0474b20
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-29 01:25:53 +0200

    day_11/part-1: Solve Part One of Day 11

--- src/advent_of_code/day_11.clj
+++ src/advent_of_code/day_11.clj
@@ -1,9 +1,22 @@
-(ns advent-of-code.day-11)
+(ns advent-of-code.day-11
+  (:require [clojure.string :refer [split]]))
+
+(def adjacency {"n"   [1  1  0]
+                "s"  [-1 -1  0]
+                "ne"  [1  0  1]
+                "sw" [-1  0 -1]
+                "nw"  [0  1 -1]
+                "se"  [0 -1  1]})
+
+(defn move
+  [[x y z] baby-step] (let [[dx dy dz] (get adjacency baby-step)]
+                        [(+ x dx) (+ y dy) (+ z dz)]))
 
 (defn part-1
   "Day 11 Part 1"
-  [input]
-  input)
+  [input] (let [path (split input #",")
+                pos (reduce move [0 0 0] path)]
+            (/ (apply + (map #(Math/abs %) pos)) 2)))
 
 (defn part-2
   "Day 11 Part 2"
--- test/advent_of_code/day_11_test.clj
+++ test/advent_of_code/day_11_test.clj
@@ -3,10 +3,23 @@
             [advent-of-code.day-11 :refer [part-1 part-2]]
             [clojure.java.io :refer [resource]]))
 
-(deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-11-example.txt")))))))
+(deftest part-1-example-1
+  (is (= 3 (part-1 "ne,ne,ne"))))
+
+(deftest part-1-example-2
+  (is (= 0 (part-1 "ne,ne,sw,sw"))))
+
+(deftest part-1-example-3
+  (is (= 2 (part-1 "ne,ne,s,s"))))
+
+(deftest part-1-example-4
+  (is (= 3 (part-1 "se,sw,se,sw,sw"))))
+
+(deftest part-1-example-5
+  (is (= 2 (part-1 "ne,ne,nw,nw"))))
+
+(deftest part-1-example-6
+  (is (= 2 (part-1 "se,sw,se,sw"))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-11-example.txt")))))))
+  (is (= "" (part-2 ""))))
