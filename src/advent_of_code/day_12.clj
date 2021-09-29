117ee0ed7dac03cd6d69e8d43c852aa62d1b56d4
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-29 14:05:10 +0200

    day_12/part-1: Solve Part One of Day 12

--- src/advent_of_code/day_12.clj
+++ src/advent_of_code/day_12.clj
@@ -1,9 +1,30 @@
 (ns advent-of-code.day-12)
 
+(defn parse-village
+  "Parses input into a map representation of the village"
+  [input] (->>
+           (re-seq #"[^\n]+" input)
+           (map (partial re-seq #"\d+"))
+           (map (fn [[prog & comm]] [prog comm]))
+           (into {})))
+
+(defn visit-all
+  "Visits all programs reachable from a starting program"
+  [village start]
+  (loop [seen #{start}
+         next-progs (into '() [start])]
+    (if (empty? next-progs)
+      seen
+      (let [[prog & next-progs] next-progs]
+        (recur (conj seen prog)
+               (reduce
+                #(if (contains? seen %2) %1 (conj %1 %2))
+                next-progs
+                (get village prog)))))))
+
 (defn part-1
   "Day 12 Part 1"
-  [input]
-  input)
+  [input] (count (visit-all (parse-village input) "0")))
 
 (defn part-2
   "Day 12 Part 2"
