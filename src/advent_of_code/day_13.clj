b7ebdf06be411e02d67a4b928c51fd8b52e76899
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-29 23:29:10 +0200

    day_13/part-1: Solve Part One of Day 13

--- src/advent_of_code/day_13.clj
+++ src/advent_of_code/day_13.clj
@@ -1,9 +1,21 @@
 (ns advent-of-code.day-13)
 
+(defn parse-record
+  "Parses input into a map representation of the firewall record"
+  [input] (->>
+           (re-seq #"\d+" input)
+           (map #(Integer/parseInt %))
+           (partition 2)))
+
+(defn trip-severity
+  "Calculates the severity of the whole trip"
+  [severity [depth range]]
+  (let [full-cycle (* (dec range) 2)]
+    (if (zero? (mod depth full-cycle)) (+ severity (* depth range)) severity)))
+
 (defn part-1
   "Day 13 Part 1"
-  [input]
-  input)
+  [input] (reduce trip-severity 0 (parse-record input)))
 
 (defn part-2
   "Day 13 Part 2"
