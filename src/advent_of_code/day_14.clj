46a93e4eab35583eca0d00f0d1cd8e1ee0be60b6
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-10-01 20:43:28 +0200

    day_14/part-1: Solve Part One of Day 14

--- src/advent_of_code/day_14.clj
+++ src/advent_of_code/day_14.clj
@@ -1,9 +1,24 @@
-(ns advent-of-code.day-14)
+(ns advent-of-code.day-14
+  (:require [clojure.pprint :refer (cl-format)]
+            [clojure.string :refer [join]]
+            [advent-of-code.day-10 :as day-10]))
+
+(defn hash->grid
+  [hash] (->>
+          (map #(cl-format nil "~4,'0',B" (Integer/parseInt (str %) 16)) hash)
+          (join)
+          (vec)))
 
 (defn part-1
   "Day 14 Part 1"
-  [input]
-  input)
+  [input] (->>
+           (map vector (cycle [input]) (range 128))
+           (map (partial join "-"))
+           (map day-10/part-2)
+           (mapv hash->grid)
+           (flatten)
+           (filterv #(= % \1))
+           (count)))
 
 (defn part-2
   "Day 14 Part 2"
--- test/advent_of_code/day_14_test.clj
+++ test/advent_of_code/day_14_test.clj
@@ -1,12 +1,11 @@
 (ns advent-of-code.day-14-test
   (:require [clojure.test :refer [deftest is]]
             [advent-of-code.day-14 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :refer [trimr]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-14-example.txt")))))))
+  (is (= 8108 (part-1 (trimr (slurp (resource "day-14-example.txt")))))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-14-example.txt")))))))
+  (is (not= "" (part-2 (trimr (slurp (resource "day-14-example.txt")))))))
