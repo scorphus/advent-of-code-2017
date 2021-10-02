94189261fc5b80dee3c3d0728a3879a7a3bff9ae
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-10-02 02:37:49 +0200

    day_15/part-1: Solve Part One of Day 15

--- src/advent_of_code/day_15.clj
+++ src/advent_of_code/day_15.clj
@@ -1,9 +1,25 @@
 (ns advent-of-code.day-15)
 
+(def a-factor 16807)
+(def b-factor 48271)
+(def divider 2147483647)
+(def mask 65535)
+
 (defn part-1
   "Day 15 Part 1"
-  [input]
-  input)
+  [input] (->>
+           (re-seq #"\d+" input)
+           (map #(Integer/parseInt %))
+           (#(reduce
+              (fn [[[a b] matches] _]
+                (let [a' (mod (* a a-factor) divider)
+                      b' (mod (* b b-factor) divider)
+                      a'' (bit-and a mask)
+                      b'' (bit-and b mask)]
+                  [[a' b'] (if (= a'' b'') (inc matches) matches)]))
+              [% 0]
+              (range 4e7)))
+           (second)))
 
 (defn part-2
   "Day 15 Part 2"
