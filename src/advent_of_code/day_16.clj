1cf847aee0899f43704a0f9cd39bb6f752ef1fbf
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-10-02 20:44:56 +0200

    day_16/part-1: Solve Part One of Day 16

--- src/advent_of_code/day_16.clj
+++ src/advent_of_code/day_16.clj
@@ -1,9 +1,40 @@
-(ns advent-of-code.day-16)
+(ns advent-of-code.day-16
+  (:require [clojure.string :refer [join split]]))
+
+(defn draft-programs [n]
+  (mapv char (range (int \a) (+ (int \a) n))))
+
+(defn spin
+  [programs dance-move]
+  (let [[x'] (re-seq #"\d+" dance-move)
+        x (- (count programs) (Integer/parseInt x'))]
+    (into [] (take (count programs) (drop x (cycle programs))))))
+
+(defn exchange
+  ([programs dance-move]
+   (let [[a b] (map #(Integer/parseInt %) (re-seq #"\d+" dance-move))]
+     (exchange programs a b)))
+  ([programs a b]
+   (assoc (assoc programs b (get programs a)) a (get programs b))))
+
+(defn partner [programs dance-move]
+  (let [[_ [a] [b]] (map vec (first (re-seq #"(.)/(.)" dance-move)))]
+    (exchange programs (.indexOf programs a) (.indexOf programs b))))
 
 (defn part-1
   "Day 16 Part 1"
-  [input]
-  input)
+  ([input] (part-1 input 16))
+  ([input total]
+   (let [dance-moves (split input #",")]
+     (join
+      (reduce
+       (fn [programs dance-move]
+         (case (first dance-move)
+           \s (spin programs dance-move)
+           \x (exchange programs dance-move)
+           (partner programs dance-move)))
+       (draft-programs total)
+       dance-moves)))))
 
 (defn part-2
   "Day 16 Part 2"
--- test/advent_of_code/day_16_test.clj
+++ test/advent_of_code/day_16_test.clj
@@ -1,12 +1,11 @@
 (ns advent-of-code.day-16-test
   (:require [clojure.test :refer [deftest is]]
             [advent-of-code.day-16 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :refer [trimr]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-16-example.txt")))))))
+  (is (= "baedc" (part-1 (trimr (slurp (resource "day-16-example.txt"))) 5))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-16-example.txt")))))))
+  (is (not= "" (part-2 (trimr (slurp (resource "day-16-example.txt")))))))
