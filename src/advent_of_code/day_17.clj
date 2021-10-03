afd523cb2d932853c781a5af33c4944de02b3ba1
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-10-03 21:59:47 +0200

    day_17/part-1: Solve Part One of Day 17

--- src/advent_of_code/day_17.clj
+++ src/advent_of_code/day_17.clj
@@ -3,7 +3,13 @@
 (defn part-1
   "Day 17 Part 1"
   [input]
-  input)
+  (let [steps (Integer/parseInt (first (re-seq #"\d+" input)))]
+    (second
+     (reduce
+      (fn [buffer value]
+        (conj (take value (drop (inc steps) (cycle buffer))) value))
+      (list)
+      (range 2018)))))
 
 (defn part-2
   "Day 17 Part 2"
--- test/advent_of_code/day_17_test.clj
+++ test/advent_of_code/day_17_test.clj
@@ -4,9 +4,7 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-17-example.txt")))))))
+  (is (= 638 (part-1 (slurp (resource "day-17-example.txt"))))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-17-example.txt")))))))
+  (is (not= "" (part-2 (slurp (resource "day-17-example.txt"))))))
