85f207abe7ff584d59f004685c1650891c595a80
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-10-09 20:41:07 +0200

    day_20/part-1: Solve Part One of Day 20

--- src/advent_of_code/day_20.clj
+++ src/advent_of_code/day_20.clj
@@ -1,9 +1,32 @@
 (ns advent-of-code.day-20)
 
+(defn parse-particles
+  [input] (->>
+           (re-seq #"[^\n]+" input)
+           (map #(re-seq #"[\d-]+" %))
+           (map (partial map #(Integer/parseInt %)))
+           (map-indexed #(vector %1 (partition 3 %2)))))
+
+(defn pos [t p v a] (+ p (* v t) (* a t (inc t) 1/2)))
+
+(defn pos-xyz
+  [t [p v a]]
+  (loop [[pᵢ & p] p
+         [vᵢ & v] v
+         [aᵢ & a] a
+         res []]
+    (if (nil? pᵢ) res (recur p v a (conj res (pos t pᵢ vᵢ aᵢ))))))
+
+(defn distance-after
+  [t [_ [p v a]]] (reduce + (map #(Math/abs %) (pos-xyz t [p v a]))))
+
 (defn part-1
   "Day 20 Part 1"
   [input]
-  input)
+  (->>
+   (parse-particles input)
+   (apply min-key #(distance-after 1e6 %))
+   (first)))
 
 (defn part-2
   "Day 20 Part 2"
--- test/advent_of_code/day_20_test.clj
+++ test/advent_of_code/day_20_test.clj
@@ -1,12 +1,11 @@
 (ns advent-of-code.day-20-test
   (:require [clojure.test :refer [deftest is]]
             [advent-of-code.day-20 :refer [part-1 part-2]]
-            [clojure.java.io :refer [resource]]))
+            [clojure.java.io :refer [resource]]
+            [clojure.string :refer [trim-newline]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-20-example.txt")))))))
+  (is (= 0 (part-1 (trim-newline (slurp (resource "day-20-example.txt")))))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-20-example.txt")))))))
+  (is (not= "" (part-2 (trim-newline (slurp (resource "day-20-example.txt")))))))
