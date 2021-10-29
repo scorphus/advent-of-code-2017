772b3548991a462bd0a3a7ef734153ff120f7cf4
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-10-29 20:46:34 +0200

    day_22/part-2: Solve Part Two of Day 22

--- src/advent_of_code/day_22.clj
+++ src/advent_of_code/day_22.clj
@@ -8,34 +8,56 @@
       [(first
         (reduce
          (fn [[grid r c] node]
-           [(if (= node "#") (conj grid [r c]) grid) r (inc c)])
+           [(if (= node "#") (assoc grid [r c] :infected) grid) r (inc c)])
          [grid r 0]
          (re-seq #"." row)))
        (inc r)])
-    [#{} 0]
+    [{} 0]
     (re-seq #"[^\n]+" input))))
 
-(defn turn [[Δr Δc] infected] (if infected [Δc (- Δr)] [(- Δc) Δr]))
+(defn turn
+  [[Δr Δc] node]
+  (case node
+    :weakened [Δr Δc]
+    :infected [Δc (- Δr)]
+    :flagged [(- Δr) (- Δc)]
+    [(- Δc) Δr]))
+
+(defn evolve
+  [must-evolve node]
+  (case must-evolve
+    true (case node
+           :weakened :infected
+           :infected :flagged
+           :flagged nil
+           :weakened)
+    (case node :infected nil :infected)))
 
 (defn move-in-bursts
-  ([[grid [r c] deltas infections]]
-   (let [infected (contains? grid [r c])
-         [Δr Δc :as deltas] (turn deltas infected)
-         grid (if infected (disj grid [r c]) (conj grid [r c]))
-         infections (if infected infections (inc infections))]
-     [grid [(+ r Δr) (+ c Δc)] deltas infections])))
+  ([[grid [r c] deltas must-evolve infections]]
+   (let [node (get grid [r c])
+         [Δr Δc :as deltas] (turn deltas node)
+         node' (evolve must-evolve node)
+         grid (assoc grid [r c] node')
+         infections (if (= node' :infected) (inc infections) infections)]
+     [grid [(+ r Δr) (+ c Δc)] deltas must-evolve infections])))
 
 (defn part-1
   "Day 22 Part 1"
   [input]
   (let [grid (parse-grid input)
-        mid-point (quot (inc (apply max (flatten (vec grid)))) 2)]
+        mid-point (quot (inc (apply max (flatten (keys grid)))) 2)]
     (->
-     (iterate move-in-bursts [grid [mid-point mid-point] [-1 0] 0])
+     (iterate move-in-bursts [grid [mid-point mid-point] [-1 0] false 0])
      (nth 10000)
      (last))))
 
 (defn part-2
   "Day 22 Part 2"
   [input]
-  input)
+  (let [grid (parse-grid input)
+        mid-point (quot (inc (apply max (flatten (keys grid)))) 2)]
+    (->
+     (iterate move-in-bursts [grid [mid-point mid-point] [-1 0] true 0])
+     (nth 10000000)
+     (last))))
--- test/advent_of_code/day_22_test.clj
+++ test/advent_of_code/day_22_test.clj
@@ -1,23 +1,45 @@
 (ns advent-of-code.day-22-test
   (:require [clojure.test :refer [deftest is]]
-            [advent-of-code.day-22 :refer [part-1 part-2 turn]]
+            [advent-of-code.day-22 :refer [part-1 part-2 turn evolve]]
             [clojure.java.io :refer [resource]]
             [clojure.string :refer [trim-newline]]))
 
 (deftest turn-right
-  (is (= [0, 1] (turn [-1, 0] true)))
-  (is (= [1, 0] (turn [0, 1] true)))
-  (is (= [0, -1] (turn [1, 0] true)))
-  (is (= [-1, 0] (turn [0, -1] true))))
+  (is (= [0, 1] (turn [-1, 0] :infected)))
+  (is (= [1, 0] (turn [0, 1] :infected)))
+  (is (= [0, -1] (turn [1, 0] :infected)))
+  (is (= [-1, 0] (turn [0, -1] :infected))))
 
 (deftest turn-left
-  (is (= [0, -1] (turn [-1, 0] false)))
-  (is (= [1, 0] (turn [0, -1] false)))
-  (is (= [0, 1] (turn [1, 0] false)))
-  (is (= [-1, 0] (turn [0, 1] false))))
+  (is (= [0, -1] (turn [-1, 0] nil)))
+  (is (= [1, 0] (turn [0, -1] nil)))
+  (is (= [0, 1] (turn [1, 0] nil)))
+  (is (= [-1, 0] (turn [0, 1] nil))))
+
+(deftest turn-not
+  (is (= [-1, 0] (turn [-1, 0] :weakened)))
+  (is (= [0, -1] (turn [0, -1] :weakened)))
+  (is (= [1, 0] (turn [1, 0] :weakened)))
+  (is (= [0, 1] (turn [0, 1] :weakened))))
+
+(deftest turn-back
+  (is (= [1, -0] (turn [-1, 0] :flagged)))
+  (is (= [-0, 1] (turn [0, -1] :flagged)))
+  (is (= [-1, -0] (turn [1, 0] :flagged)))
+  (is (= [-0, -1] (turn [0, 1] :flagged))))
+
+(deftest evolve-should-not-evolve
+  (is (= :infected (evolve false nil)))
+  (is (= nil (evolve false :infected))))
+
+(deftest evolve-should-evolve
+  (is (= :weakened (evolve true nil)))
+  (is (= :infected (evolve true :weakened)))
+  (is (= :flagged (evolve true :infected)))
+  (is (= nil (evolve true :flagged))))
 
 (deftest part-1-example
   (is (= 5587 (part-1 (trim-newline (slurp (resource "day-22-example.txt")))))))
 
 (deftest part-2-example
-  (is (not= "" (part-2 (trim-newline (slurp (resource "day-22-example.txt")))))))
+  (is (= 2511944 (part-2 (trim-newline (slurp (resource "day-22-example.txt")))))))
