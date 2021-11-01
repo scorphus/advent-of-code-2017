899776910ba209deeb16535173e82a6366afa5b9
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-11-01 15:18:20 +0100

    day_24/part-1: Solve Part One of Day 24

--- src/advent_of_code/day_24.clj
+++ src/advent_of_code/day_24.clj
@@ -1,9 +1,32 @@
 (ns advent-of-code.day-24)
 
+(defn parse-port [ports port]
+  (let [[p1 p2] (map #(Integer/parseInt %) (re-seq #"\d+" port))]
+    (update-in
+     (update-in
+      ports [p1] (fnil conj #{}) p2)
+     [p2] (fnil conj #{}) p1)))
+
+(defn parse-ports [input] (reduce parse-port {} (re-seq #"[^\n]+" input)))
+
+(defn strongest-bridge
+  ([ports] (strongest-bridge ports 0 #{}))
+  ([ports p1 seen]
+   (loop [next-ports (get ports p1)
+          strength 0]
+     (if (empty? next-ports)
+       strength
+       (let [[p2 & next-ports] next-ports
+             pair (set [p1 p2])]
+         (if (contains? seen pair)
+           (recur next-ports strength)
+           (let [strength´ (+ p1 p2 (strongest-bridge ports p2 (conj seen pair)))]
+             (recur next-ports (max strength´ strength)))))))))
+
 (defn part-1
   "Day 24 Part 1"
   [input]
-  input)
+  (strongest-bridge (parse-ports input)))
 
 (defn part-2
   "Day 24 Part 2"
--- test/advent_of_code/day_24_test.clj
+++ test/advent_of_code/day_24_test.clj
@@ -4,9 +4,31 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part-1-example
-  (let [expected ""]
-    (is (= expected (part-1 (slurp (resource "day-24-example.txt")))))))
+  (is (= 31 (part-1 (slurp (resource "day-24-example.txt"))))))
+
+(deftest part-1-example-1
+  (is (= 65 (part-1 "
+0/5
+0/2
+2/2
+2/21
+9/11
+11/5
+21/4
+21/15
+"))))
+
+(deftest part-1-example-2
+  (is (= 65 (part-1 "
+0/1
+0/2
+2/2
+2/21
+9/11
+11/1
+21/4
+21/15
+"))))
 
 (deftest part-2-example
-  (let [expected ""]
-    (is (= expected (part-2 (slurp (resource "day-24-example.txt")))))))
+  (is (not= "" (part-2 (slurp (resource "day-24-example.txt"))))))
